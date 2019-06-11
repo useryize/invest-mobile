24e550a4533542f733bd93a8da5628654cf0d7ff
YIZE <136076063@qq.com>
2019-06-11 17:57:55 +0800

    less全局属性

--- src/container/main/index.vue
+++ src/container/main/index.vue
@@ -12,3 +12,6 @@ export default {
     }
 };
 </script>
+<style lang="less">
+ @import './main.less';
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import App from './container/index.vue';
+import App from './container/main';
 import Home from './container/home';
 import List from './container/list';
 import 'lib-flexible';
